a7c30d9c630dbaf5af2743437e70dee8e2eaefee
Tap84 <43452884+Tap84@users.noreply.github.com>
2019-11-04 18:45:32 -0500

    primative disc additions

--- bot.py
+++ bot.py
@@ -18,23 +18,33 @@ conn.execute(''' CREATE TABLE IF NOT EXISTS prodigy_discs(
 #                       Tyler               Dylan               Nick                Zane                Skaffen             Deck
 authorized_users = [94953298645225472, 428298884998234112, 275626705475862530, 99017591317610496, 507227977394946048, 98607564257771520]
 
-
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+    
     if message.author == client.user:
         return
 
     if message.content.startswith('.disc'):
-        await search_disc_by_name(message.content[6:],message)
-        
+        try:
+            await search_disc_by_name(message.content[6:],message)
+        except:
+            await message.channel.send("Search Failed")
     
-    if message.content.startswith('.adddisc') and message.author in authorized_users:
+    if message.content.startswith('.adddisc') and message.author.id in authorized_users:
         discinfo = message.content[8:].split()
-        await add_disc(discinfo, message)                                 
+        try:
+            await add_disc(discinfo, message)
+        except:
+            await message.channel.send("Failed")
+    elif message.content.startswith('adddisc'):
+        await message.channel.send("Unauthorized")                     
                                             
 async def add_disc(discinfo, message):
-    conn.execute(''' INSERT INTO prodigy_discs(NAME,CLASS,SPEED,GLIDE,TURN,FADE)
+    conn.execute(''' INSERT INTO discs(NAME,MANUFACTURER,SPEED,GLIDE,TURN,FADE)
                         VALUES(?,?,?,?,?,?)''',    (discinfo[0],
                                                     discinfo[1],
                                                     discinfo[2],
@@ -49,9 +59,9 @@ async def search_disc_by_name(name, message):
     
     out_message = "```"
     cur = conn.cursor()
-    cur.execute(f''' SELECT * FROM prodigy_discs WHERE NAME=?''',(name,))
+    cur.execute(f''' SELECT * FROM discs WHERE NAME=?''',(name,))
     disc = cur.fetchall()[0]
-    out_message += f'''Name: {disc[0]}\nClass: {disc[1]}\nSpeed: {disc[2]}\nGlide: {disc[3]}\nTurn: {disc[4]}\nFade: {disc[5]}'''
+    out_message += f'''Name: {disc[0]}\nManufacturer: {disc[1]}\nSpeed: {disc[2]}\nGlide: {disc[3]}\nTurn: {disc[4]}\nFade: {disc[5]}'''
     out_message += "```"
     await message.channel.send(out_message)
 
